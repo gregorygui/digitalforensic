9e8befb1d440bec29ffdf9eaa0b1f9febe564ae3
Grégory <gregory.guillermin@postgrad.manchester.ac.uk>
2017-03-17 17:45:33 +0000

    Scraping work on Virus Total response - need detailed informations from sandboxing

--- VTLibrary.py
+++ VTLibrary.py
@@ -12,13 +12,45 @@ def getVTReport(hash):
 	response = requests.post('https://www.virustotal.com/vtapi/v2/file/report', params=params)
 	return response.json()
 
+def getSpecificTag(text, sp):
+	for info in sp:
+		if info.contents[1].find(text) > 0:
+			break
+	return info
+
+def getImports(sp):
+	info=getSpecificTag("PE Imports", sp)
+	s=""
+
+	for c in info.findAllNext('a'):
+		s+=str(c.string)
+
+	s=s.split("[+]")
+	c=s[len(s)-1].split('dll')[0]
+	c+="dll"
+	s=s[1:(len(s)-1)]
+	s.append(c)
+	return s
+
+def getFilenames(sp):
+	info=getSpecificTag("VirusTotal metadata", sp)
+	s=""
+	for c in info.findAllNext('td')[1]:
+		s+=str(c).strip() 	
+	return s.split("<br/>")[:-1]
+
+def getOpenFiles(sp):
+	info = getSpecificTag("Opened files",sp)
+	for c in info.findAllNext('div', ('class','enum')):
+		print(c.contents)
+
 def getDetailed(res):
 	soup = BeautifulSoup(res, "lxml")
 	base = soup.find_all('h5')
 
-	for info in base:
-		print(info.contents[1])
-
+	imports=getImports(base)
+	filenames=getFilenames(base)
+	openfiles=getOpenFiles(base)
 	return 0
 
 def scanVT(filename):
@@ -38,17 +70,10 @@ def requestVT(filename):
 		current = scanVT(filename)
 		return current['permalink']
 
-def extractInfo(url):
+def extractInfo():
+	tmp=open('tmp.txt','r')
 	p=0
 	res=""
-	params = {'apikey': KEY}
-	response = requests.post(url, params=params)
-	
-	tmp=open('tmp.txt','w')
-	tmp.write(response.text)
-	tmp.close()
-
-	tmp=open('tmp.txt','r')
 
 	for line in tmp:
 		if line.find("file-details") > 0:
@@ -57,15 +82,26 @@ def extractInfo(url):
 			p=0
 		if p > 0:
 			res+=line 
+	
 	tmp.close()
+	return res
+
+def extractWebPage(url):
+	params = {'apikey': KEY}
+	response = requests.post(url, params=params)
+	
+	tmp=open('tmp.txt','w')
+	tmp.write(response.text)
+	tmp.close()
+
+	res=extractInfo()
 
 	return getDetailed(res)
 	
 def main():
 	filename='infected.file'
-	url=requestVT(filename)
-	print(extractInfo(url))
-	print('\n')
+	#url=requestVT(filename)
+	getDetailed(extractInfo())
 
 if __name__ == "__main__":
 	main()
