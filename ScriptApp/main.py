2094c47b613a20a7c5df773d7e4c59cf71ab9305
Grégory Guillermin <gregory.guillermin@postgrad.manchester.ac.uk>
2017-07-17 19:55:35 +0100

    few modifications switch to ssh

--- ScriptApp/main.py
+++ ScriptApp/main.py
@@ -3,11 +3,14 @@ Created on 15 juil. 2017
 
 @author: Gregory
 '''
-from VirusTotalAPI import getScore
+import yaml
 
+config=yaml.load(open('config.yml','r'))
+
+BASE=config['SAMPLES_DIRECTORY']
 
 def main():
-    print("Hello")
+    print(BASE)
 
 if __name__ == '__main__':
     main()
